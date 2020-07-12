de51172be41ace0ed7c149da3793f1f0753b35df
황선혜 <ghkdtjs6451@gmail.com>
2020-07-12 10:04:27 +0900

    방 상세 정보 표시 화면 추가

---
+++ app/src/main/java/kr/co/tjoeun/listviewpractive_20200711/ViewRoomDetailActivity.kt
@@ -0,0 +1,23 @@
+package kr.co.tjoeun.listviewpractive_20200711
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewRoomDetailActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_room_detail)
+
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
